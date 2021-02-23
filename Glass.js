e7ab476e20ac8d87e4865333e8e180718edce442
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-02-23 11:18:02 +0530

    Add files via upload

---
+++ Glass.js
@@ -0,0 +1,9 @@
+ 
+
+class Glass extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.image = loadImage("sprites/glass.png");
+  }
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# c27ref
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
